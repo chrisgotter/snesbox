bd5ab183ea03325164f063cf30d4934424df727b
justin@bozalina.com <justin@bozalina.com@1fbd0ffc-c5c4-0dea-51b7-1e84b96f8caf>
2010-09-08 04:52:14 +0000

    Implementing Scheduler.

--- Snes/Scheduler/Scheduler.cs
+++ Snes/Scheduler/Scheduler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Nall;
 
 namespace Snes.Scheduler
 {
@@ -10,13 +10,34 @@ namespace Snes.Scheduler
         public enum ExitReason : uint { UnknownEvent, FrameEvent, SynchronizeEvent, DebuggerEvent }
         public ExitReason exit_reason;
 
-        public Object host_thread; //program thread (used to exit emulation)
-        public Object thread; //active emulation thread (used to enter emulation)
+        public Cothread host_thread; //program thread (used to exit emulation)
+        public Cothread thread; //active emulation thread (used to enter emulation)
 
-        public void enter() { throw new NotImplementedException(); }
-        public void exit(ExitReason reason) { throw new NotImplementedException(); }
+        public void enter()
+        {
+            host_thread = Libco.Active();
+            Libco.Switch(thread);
+        }
 
-        public void init() { throw new NotImplementedException(); }
-        public Scheduler() { throw new NotImplementedException(); }
+        public void exit(ExitReason reason)
+        {
+            exit_reason = reason;
+            thread = Libco.Active();
+            Libco.Switch(host_thread);
+        }
+
+        public void init()
+        {
+            host_thread = Libco.Active();
+            thread = CPU.CPU.cpu.Processor.thread;
+            sync = SynchronizeMode.None;
+        }
+
+        public Scheduler()
+        {
+            host_thread = null;
+            thread = null;
+            exit_reason = ExitReason.UnknownEvent;
+        }
     }
 }
--- Snes/Video/Video.cs
+++ Snes/Video/Video.cs
@@ -1,5 +1,5 @@
-﻿
-using System;
+﻿using System;
+
 namespace Snes
 {
     class Video
