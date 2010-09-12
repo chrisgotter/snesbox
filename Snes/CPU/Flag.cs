b3d07f1e02c9582425ad04c4ac366c862f0daba4
justin@bozalina.com <justin@bozalina.com@1fbd0ffc-c5c4-0dea-51b7-1e84b96f8caf>
2010-09-12 16:48:10 +0000

    Implementing CPU.flag.

--- Snes/CPU/Flag.cs
+++ Snes/CPU/Flag.cs
@@ -10,32 +10,41 @@ namespace Snes
 
             public static explicit operator uint(Flag flag)
             {
-                throw new NotImplementedException();
+                return (uint)((Convert.ToInt32(flag.n) << 7) + (Convert.ToInt32(flag.v) << 6) + (Convert.ToInt32(flag.m) << 5) + (Convert.ToInt32(flag.x) << 4)
+                     + (Convert.ToInt32(flag.d) << 3) + (Convert.ToInt32(flag.i) << 2) + (Convert.ToInt32(flag.z) << 1) + (Convert.ToInt32(flag.c) << 0));
             }
 
             public uint Assign(byte data)
             {
-                throw new NotImplementedException();
+                n = Convert.ToBoolean(data & 0x80); 
+                v = Convert.ToBoolean(data & 0x40);
+                m = Convert.ToBoolean(data & 0x20); 
+                x = Convert.ToBoolean(data & 0x10);
+                d = Convert.ToBoolean(data & 0x08); 
+                i = Convert.ToBoolean(data & 0x04); 
+                z = Convert.ToBoolean(data & 0x02);
+                c = Convert.ToBoolean(data & 0x01);
+                return data;
             }
 
             public static uint operator |(Flag flag, uint data)
             {
-                throw new NotImplementedException();
+                return flag.Assign((byte)((uint)flag | data));
             }
 
             public static uint operator ^(Flag flag, uint data)
             {
-                throw new NotImplementedException();
+                return flag.Assign((byte)((uint)flag ^ data));
             }
 
             public static uint operator &(Flag flag, uint data)
             {
-                throw new NotImplementedException();
+                return flag.Assign((byte)((uint)flag & data));
             }
 
             public Flag()
             {
-                throw new NotImplementedException();
+                n = v = m = x = d = i = z = c = false;
             }
         }
     }
