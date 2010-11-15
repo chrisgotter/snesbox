3aa5051047ff6e5a5b274af99f33835e1874e58d
justin@bozalina.com <justin@bozalina.com@1fbd0ffc-c5c4-0dea-51b7-1e84b96f8caf>
2010-11-15 02:47:12 +0000

    Implementing performance PPU.LayerWindow

--- SnesBox/trunk/Snes/Performance/PPU/LayerWindow.cs
+++ SnesBox/trunk/Snes/Performance/PPU/LayerWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Nall;
 
 namespace Snes
@@ -20,8 +21,91 @@ namespace Snes
             public byte[] main = new byte[256];
             public byte[] sub = new byte[256];
 
-            public void render(bool screen) { throw new NotImplementedException(); }
-            public void serialize(Serializer s) { throw new NotImplementedException(); }
+            public void render(bool screen)
+            {
+                byte[] output;
+                if (screen == Convert.ToBoolean(0))
+                {
+                    output = main;
+                    if (main_enable == false)
+                    {
+                        Array.Copy(Enumerable.Repeat(0, 256).ToArray(), output, 256);
+                        return;
+                    }
+                }
+                else
+                {
+                    output = sub;
+                    if (sub_enable == false)
+                    {
+                        Array.Copy(Enumerable.Repeat(0, 256).ToArray(), output, 256);
+                        return;
+                    }
+                }
+
+                if (one_enable == false && two_enable == false)
+                {
+                    Array.Copy(Enumerable.Repeat(0, 256).ToArray(), output, 256);
+                    return;
+                }
+
+                if (one_enable == true && two_enable == false)
+                {
+                    bool set = Convert.ToBoolean(1) ^ one_invert, clr = !set;
+                    for (uint x = 0; x < 256; x++)
+                    {
+                        output[x] = Convert.ToByte((x >= ppu.regs.window_one_left && x <= ppu.regs.window_one_right) ? set : clr);
+                    }
+                    return;
+                }
+
+                if (one_enable == false && two_enable == true)
+                {
+                    bool set = Convert.ToBoolean(1) ^ two_invert, clr = !set;
+                    for (uint x = 0; x < 256; x++)
+                    {
+                        output[x] = Convert.ToByte((x >= ppu.regs.window_two_left && x <= ppu.regs.window_two_right) ? set : clr);
+                    }
+                    return;
+                }
+
+                for (uint x = 0; x < 256; x++)
+                {
+                    bool one_mask = (x >= ppu.regs.window_one_left && x <= ppu.regs.window_one_right) ^ one_invert;
+                    bool two_mask = (x >= ppu.regs.window_two_left && x <= ppu.regs.window_two_right) ^ two_invert;
+                    switch (mask)
+                    {
+                        case 0:
+                            output[x] = Convert.ToByte(one_mask | two_mask == Convert.ToBoolean(1));
+                            break;
+                        case 1:
+                            output[x] = Convert.ToByte(one_mask & two_mask == Convert.ToBoolean(1));
+                            break;
+                        case 2:
+                            output[x] = Convert.ToByte(one_mask ^ two_mask == Convert.ToBoolean(1));
+                            break;
+                        case 3:
+                            output[x] = Convert.ToByte(one_mask ^ two_mask == Convert.ToBoolean(0));
+                            break;
+                    }
+                }
+            }
+
+            public void serialize(Serializer s)
+            {
+                s.integer(one_enable, "one_enable");
+                s.integer(one_invert, "one_invert");
+                s.integer(two_enable, "two_enable");
+                s.integer(two_invert, "two_invert");
+
+                s.integer(mask, "mask");
+
+                s.integer(main_enable, "main_enable");
+                s.integer(sub_enable, "sub_enable");
+
+                s.array(main, "main");
+                s.array(sub, "sub");
+            }
         }
     }
 }
